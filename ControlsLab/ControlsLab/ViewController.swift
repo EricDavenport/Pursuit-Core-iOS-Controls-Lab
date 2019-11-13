99a87d2b758b6130ea300a241d2de8fb149df5cd
Eric Davenport <EricDavenport@pursuit.org>
2019-11-13 10:04:19 -0500

    storyboard placed - stepper and segment switch values coded

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -1,10 +1,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var suitSelector: UISegmentedControl!
+    
+    @IBOutlet weak var valueStepper: UIStepper!
+    
+    var suitSegmentIndex: Int = 0 {
+        didSet {
+            switch suitSelector.selectedSegmentIndex {
+            case 0:
+                ♠️
+            case 1:
+                ♥️
+            case 2:
+                ♣️
+            case 3:
+                ♦️
+            }
+            
+            
+        }
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        stepperConfigure()
+
     }
+    
+    func stepperConfigure() {
+        
+        valueStepper.maximumValue = 13
+        valueStepper.minimumValue = 1
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
